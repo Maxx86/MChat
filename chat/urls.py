7de6e4c666294e28ee2054d06dd87d8bc20bca20
Maxx <bmax86@gmail.com>
2025-11-07 21:57:55 +0200

    Start

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.chat_room, name='chat_room'),
+    path('register/', views.register, name='register'),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 @login_required
 def chat_room(request):
     return render(request, 'chat/room.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
